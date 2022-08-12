1f95825fef0c504c558e20921b0ae63ed9160151
Szymon Bacanski <s.bacanski@gmail.com>
2022-08-12 23:30:37 +0200

    'Button' as universal component
    Base of the universal Button component. {css} and destructuring props in styled-component.

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { ButtonStyle } from './Button.style';
 
-const Button = ({ children }) => {
+const Button = ({ children, btn_style, btn_size, btn_hover }) => {
+	const STYLES = ['default', 'no-outline'];
+	const SIZES = ['default', 'wide'];
+	const HOVERS = ['default'];
+
+	const checkPropStyle = STYLES.includes(btn_style) ? btn_style : STYLES[0];
+	const checkPropSize = SIZES.includes(btn_size) ? btn_size : SIZES[0];
+	const checkPropHover = HOVERS.includes(btn_hover) ? btn_hover : HOVERS[0];
+
 	return (
 		<>
-			<ButtonStyle>{children}</ButtonStyle>
+			<ButtonStyle
+				btn_style={checkPropStyle}
+				btn_size={checkPropSize}
+				btn_hover={checkPropHover}
+			>
+				{children}
+			</ButtonStyle>
 		</>
 	);
 };
--- src/Components/Button/Button.style.js
+++ src/Components/Button/Button.style.js
@@ -1,11 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const ButtonStyle = styled.button`
-	letter-spacing: 1px;
 	font-size: 1.5rem;
 	font-family: 'Montserrat', sans-serif;
-	/* font-family: 'Courier New', Courier, monospace; */
 	outline: none;
 	border: none;
 	background: none;
+
+	${({ btn_style }) => {
+		if (btn_style === 'default') {
+			return css`
+				background-color: orange;
+			`;
+		}
+	}};
+
+	${({ btn_size }) => {
+		if (btn_size === 'no-outline') {
+			return css`
+				background-color: blue;
+			`;
+		}
+	}}
+
+	${({ btn_hover }) => {
+		if (btn_hover === 'default') {
+			return css`
+				&:hover {
+					cursor: pointer;
+				}
+			`;
+		}
+	}}
 `;
