50805de388332ed04429034ab1b4096b63a2ec32
Szymon Bacanski <s.bacanski@gmail.com>
2023-01-23 13:40:49 +0100

    pages deleted - project cleaned

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Navbar from './components/NavBar/Navbar';
-import PageContainer from './components/PageContainer/PageContainer.jsx';
-import LoginPage from './pages/LoginPage/LoginPage.jsx';
-import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
-import ReportsPage from './pages/ReportsPage/ReportsPage.jsx';
-
 function App() {
-	return (
-		<>
-			<Navbar />
-			<PageContainer>
-				<ReportsPage />
-			</PageContainer>
-		</>
-	);
+	return <>Hello World</>;
 }
 
 export default App;
