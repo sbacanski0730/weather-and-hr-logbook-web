239d371124f91ebe57d5df307f728155131c84b7
Szymon Bacanski <s.bacanski@gmail.com>
2023-04-18 15:13:23 +0200

    Hotfix after auth pull request
    Hotfix contains some changes in authFunction whish now throws Error objects and problem with ThemeContext has been fixed

--- src/utils/authFunction.js
+++ src/utils/authFunction.js
@@ -1,74 +1,74 @@
 import { API_ROUTES } from './constants';
 
 export function storeTokenInLocalStorage(token) {
-	localStorage.setItem('token', token);
+    localStorage.setItem('token', token);
 }
 
 export function getTokenFromLocalStorage() {
-	return localStorage.getItem('token');
+    return localStorage.getItem('token');
 }
 
 export async function authenticateUserToken() {
-	let defaultReturnObject = { verify: false, user: null };
+    const defaultReturnObject = { verify: false, user: null };
 
-	const token = getTokenFromLocalStorage();
+    const token = getTokenFromLocalStorage();
 
-	if (!token) {
-		return defaultReturnObject;
-	}
+    if (!token) {
+        return defaultReturnObject;
+    }
 
-	const { content } = await fetch(API_ROUTES.GET_USER, {
-		method: 'GET',
-		headers: {
-			token: getTokenFromLocalStorage(),
-		},
-	}).then(res => res.json());
+    const { content } = await fetch(API_ROUTES.GET_USER, {
+        method: 'GET',
+        headers: {
+            token: getTokenFromLocalStorage(),
+        },
+    }).then((res) => res.json());
 
-	return { verify: true, user: content.email };
+    return { verify: true, user: content.email };
 }
 
 export function isValidateLogin(email, password) {
-	const emailRegex =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	if (!email || !email.toLowerCase().match(emailRegex)) {
-		throw 'Valid email required!';
-	}
+    if (!email || !email.toLowerCase().match(emailRegex)) {
+        throw new Error('Valid email required!');
+    }
 
-	if (!password) {
-		throw 'Password required!';
-	}
+    if (!password) {
+        throw new Error('Password required!');
+    }
 
-	return true;
+    return true;
 }
 
 export function isValidateRegister(email, password, repeatedPassword) {
-	const emailRegex =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	if (!email || !email.toLowerCase().match(emailRegex)) {
-		throw 'Valid email required!';
-	}
+    if (!email || !email.toLowerCase().match(emailRegex)) {
+        throw new Error('Valid email required!');
+    }
 
-	if (email.length > 128) {
-		throw 'Email too long!';
-	}
+    if (email.length > 128) {
+        throw new Error('Email too long!');
+    }
 
-	if (!password) {
-		throw 'Password required!';
-	}
+    if (!password) {
+        throw new Error('Password required!');
+    }
 
-	if (password.length < 5) {
-		throw 'Password must be at least 5 characters long!';
-	}
+    if (password.length < 5) {
+        throw new Error('Password must be at least 5 characters long!');
+    }
 
-	if (password.length > 128) {
-		throw 'Password too long!';
-	}
+    if (password.length > 128) {
+        throw new Error('Password too long!');
+    }
 
-	if (!(password === repeatedPassword)) {
-		throw 'Passwords should be the same!';
-	}
+    if (!(password === repeatedPassword)) {
+        throw new Error('Passwords should be the same!');
+    }
 
-	return true;
+    return true;
 }
