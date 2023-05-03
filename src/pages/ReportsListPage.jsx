cce8a48289709d0e8da5af1e288f75e44ce5b130
Szymon Bacanski <72625642+sbacanski0730@users.noreply.github.com>
2023-05-03 20:08:17 +0200

    #58-spinner-and-info-about-empty-list
    Add new feature with spinner during loading and info about empty reports list.

--- src/pages/ReportsListPage.jsx
+++ src/pages/ReportsListPage.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
-// import { DataGrid } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
 import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 
 import Container from '@mui/material/Container';
-import { API_ROUTES } from '../utils/constants';
+import Grow from '@mui/material/Grow';
+import CircularProgress from '@mui/material/CircularProgress';
+import { API_ROUTES, APP_ROUTES } from '../utils/constants';
 import ShowReportButton from '../components/ShowReportButton/ShowReportButton.jsx';
+import CustomButton from '../components/styled-components/CustomButton.jsx';
 
 const ReportsListPage = () => {
-    // TODO: set the max width to unable the scrollbar to list of the reports (needs to set the maxWidth)
+    const navigate = useNavigate();
 
     const [userReports, setUserReports] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const fetchReports = async () => {
+        setIsLoading(true);
         const response = await fetch(API_ROUTES.REPORTS, {
             headers: {
                 method: 'post',
@@ -21,7 +26,13 @@ const ReportsListPage = () => {
         })
             .then((res) => res.json())
             .catch((e) => console.log(e));
+        // setIsLoading(false);
         setUserReports(response.content);
+        console.log(response);
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
     };
 
     useEffect(() => {
@@ -47,43 +58,99 @@ const ReportsListPage = () => {
     }));
 
     return (
-        <Container maxWidth="xl">
-            <TableContainer sx={{ maxHeight: 440 }}>
-                <Table stickyHeader>
-                    <TableHead sx={{ border: '2px dotted orange', backgroundColor: 'red' }}>
-                        <TableRow>
-                            {columns.map((column) => (
+        <>
+            <Container
+                maxWidth="xl"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                {isLoading ? (
+                    <>
+                        <Grow in={isLoading} easing="ease-out" timeout={2000} appear={true}>
+                            <CircularProgress sx={{ color: 'secondary.dark' }} size="52px" />
+                        </Grow>
+                    </>
+                ) : (
+                    <>
+                        <TableContainer sx={{ maxHeight: 440 }}>
+                            {userReports.length === 0 ? (
                                 <>
-                                    <TableCell
-                                        key={column.label}
-                                        align="center"
+                                    <Container
+                                        maxWidth="sm"
                                         sx={{
-                                            backgroundColor: 'primary.dark',
+                                            display: 'flex',
+                                            flexDirection: 'column',
+
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            fontSize: '28px',
                                         }}
                                     >
-                                        {column.label}
-                                    </TableCell>
+                                        <p>User doesn&apos;t have any reports</p>
+                                        <CustomButton
+                                            variant="contained"
+                                            sx={{ width: '100%' }}
+                                            onClick={() => navigate(APP_ROUTES.ADD_REPORT)}
+                                        >
+                                            Add Report
+                                        </CustomButton>
+                                    </Container>
                                 </>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.id}>
-                                <TableCell align="center">{row.date.substring(0, 10)}</TableCell>
-                                <TableCell align="center">{row.title}</TableCell>
-                                <TableCell align="center">{row.skyStatus}</TableCell>
-                                <TableCell align="center">{row.shipStatus}</TableCell>
-                                <TableCell align="center">{row.windSpeed}</TableCell>
-                                <TableCell align="center">
-                                    <ShowReportButton />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Container>
+                            ) : (
+                                <Table stickyHeader>
+                                    <TableHead
+                                        sx={{ border: '2px dotted orange', backgroundColor: 'red' }}
+                                    >
+                                        <TableRow>
+                                            {columns.map((column) => (
+                                                <>
+                                                    <TableCell
+                                                        key={column.label}
+                                                        align="center"
+                                                        sx={{
+                                                            backgroundColor: 'primary.dark',
+                                                        }}
+                                                    >
+                                                        {column.label}
+                                                    </TableCell>
+                                                </>
+                                            ))}
+                                        </TableRow>
+                                    </TableHead>
+
+                                    <TableBody>
+                                        {rows.map((row) => (
+                                            <TableRow key={row.id}>
+                                                <TableCell align="center">
+                                                    {row.date.substring(0, 10)}
+                                                </TableCell>
+                                                <TableCell align="center">{row.title}</TableCell>
+                                                <TableCell align="center">
+                                                    {row.skyStatus}
+                                                </TableCell>
+                                                <TableCell align="center">
+                                                    {row.shipStatus}
+                                                </TableCell>
+                                                <TableCell align="center">
+                                                    {row.windSpeed}
+                                                </TableCell>
+                                                <TableCell align="center">
+                                                    <ShowReportButton />
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            )}
+                        </TableContainer>
+                    </>
+                )}
+            </Container>
+        </>
     );
 };
 
